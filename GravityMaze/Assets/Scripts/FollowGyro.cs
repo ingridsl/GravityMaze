ef48c737443205c32386cdefe0dedf36b68200b9
Ingrid <inus2agome@gmail.com>
2020-11-06 14:30:00 +0900

    CORRECTION2

---
+++ GravityMaze/Assets/Scripts/FollowGyro.cs
@@ -0,0 +1,62 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowGyro : MonoBehaviour
+{
+    [Header("Logic")]
+    [SerializeField] private Quaternion baseRotation = new Quaternion(0, 0, 1, 0);
+    public Quaternion gyroMovement = new Quaternion(0, 0, 1, 0);
+    private Rigidbody2D rb2d;
+    public float speed = 15;
+
+    private readonly float idleProtectionY = 0.045f;
+    private readonly float idleProtectionX = 0.045f;
+
+    private readonly float limitXY = 4.7f;
+
+    private bool canMove = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartAfterTime(Constants.COUNTDOWN_TIME));
+    }
+
+    IEnumerator StartAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        GyroManager.Instance.EnableGyro();
+        rb2d = GetComponent<Rigidbody2D>();
+
+        canMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canMove)
+        {
+            gyroMovement = GyroManager.Instance.GetGyroRotation() * baseRotation;
+            //gyro x is map y. gyro y is map x
+            float moveBallX = (Mathf.Abs(gyroMovement.y) > idleProtectionY) ?
+                                    gyroMovement.y : 0f;
+            float moveBallY = (Mathf.Abs(gyroMovement.x) > idleProtectionX) ?
+                                    gyroMovement.x : 0f;
+
+            Vector2 movement = new Vector2(-moveBallX * speed, moveBallY * speed);
+            Vector2 finalPosition = rb2d.position + movement * Time.fixedDeltaTime;
+            if (Mathf.Abs(finalPosition.y) > limitXY)
+            {
+                finalPosition.y = rb2d.position.y;
+            }
+            else if (Mathf.Abs(finalPosition.x) > limitXY)
+            {
+                finalPosition.x = rb2d.position.x;
+            }
+            rb2d.MovePosition(finalPosition);
+        }
+    }
+}
---
+++ GravityMaze/Assets/Scripts/ParallaxGyro.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxGyro : MonoBehaviour
+{
+    [Header("Logic")]
+    [SerializeField] private Quaternion baseRotation = new Quaternion(0, 0, 1, 0);
+    public Quaternion gyroMovement = new Quaternion(0, 0, 1, 0);
+    private SpriteRenderer star;
+    public float speed = 1;
+
+    private readonly float idleProtectionY = 0.06f;
+    private readonly float idleProtectionX = 0.06f;
+
+    private readonly float limitX = 12f;
+    private readonly float limitY = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GyroManager.Instance.EnableGyro();
+        star = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gyroMovement = GyroManager.Instance.GetGyroRotation() * baseRotation;
+        //gyro x is map y. gyro y is map x
+        
+        if (Mathf.Abs(gyroMovement.y) > idleProtectionY || Mathf.Abs(gyroMovement.x) > idleProtectionX)
+        {
+            float moveX = -gyroMovement.y;
+            float moveY = gyroMovement.x;
+            if (Mathf.Abs(star.transform.position.x + (moveX * speed)) > limitX)
+            {
+                moveX = 0;
+            }
+            if (Mathf.Abs(star.transform.position.y + (moveY * speed)) > limitY)
+            {
+                moveY = 0;
+            }
+            Vector2 movement = new Vector2(star.transform.position.x + (moveX * speed), star.transform.position.y + (moveY * speed));
+            star.transform.position = movement;
+        }
+    }
+}
