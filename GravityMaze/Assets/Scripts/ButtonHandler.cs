c658ce0a23654cdd679c54fc8b66480f91419b3d
Ingrid <inus2agome@gmail.com>
2020-11-18 09:51:59 +0900

    Level Selection Menu

--- GravityMaze/Assets/Scripts/ButtonHandler.cs
+++ GravityMaze/Assets/Scripts/ButtonHandler.cs
@@ -8,17 +8,63 @@ public class ButtonHandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void LoadScene(string levelName)
     {
         SceneManager.LoadScene(levelName);
+    }
+
+    public void SwitchToSelectLevelTable()
+    {
+        LevelSelectionActivation(true);
+        MainMenuActivation(false);
+    }
+
+    public void SwitchToMainMenu()
+    {
+        LevelSelectionActivation(false);
+        MainMenuActivation(true);
+    }
+
+    public void OpenSettings()
+    {
+
+    }
+
+    public void OpenInfo()
+    {
+
+    }
+
+    private void MainMenuActivation(bool isActive)
+    {
+        GameObject mainMenuObj = GameObject.Find("MainMenu");
+        if (mainMenuObj != null)
+        {
+            foreach (Transform child in mainMenuObj.transform)
+            {
+                child.gameObject.SetActive(isActive);
+            }
+        }
+    }
+
+    private void LevelSelectionActivation(bool isActive)
+    {
+        GameObject levelSelectionObj = GameObject.Find("LevelSelection");
+        if (levelSelectionObj != null)
+        {
+            foreach (Transform child in levelSelectionObj.transform)
+            {
+                child.gameObject.SetActive(isActive);
+            }
+        }
     }
 }
