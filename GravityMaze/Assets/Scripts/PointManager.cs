338259d1efac8becc5b842c68a08d1dacb888488
Ingrid <inus2agome@gmail.com>
2020-11-22 21:18:28 +0900

    Start of collectables and point counting

---
+++ GravityMaze/Assets/Scripts/PointManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointManager : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            this.enabled = false;
+            AddPoint();
+        }
+    }
+
+    public static void AddPoint()
+    {
+        var player = GameObject.Find("Ball");
+        if (player != null)
+        {
+            var playerManager = player.GetComponent<PlayerManager>();
+            if (playerManager != null)
+            {
+                playerManager.pointsOnLevel++;
+            }
+        }
+    }
+}
